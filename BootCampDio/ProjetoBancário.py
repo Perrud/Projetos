62c8b9e875c0d548ae8f5a9a3812f59acd5ef715
Perrud <viniciusperrud@gmail.com>
2024-04-21 06:26:11 -0300

    Create ProjetoBancário.py

---
+++ BootCampDio/ProjetoBancário.py
@@ -0,0 +1,39 @@
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+usuários = {}
+
+def AdicionarUsuário(nome, saldo = 0, limite = 500, extrato = "", numero_saques = 0, LIMITE_SAQUES = 3):
+    usuários[nome] = [saldo, limite, extrato, numero_saques, LIMITE_SAQUES]
+
+def Depósito(usuário, valor):
+    usuários[usuário][0] += valor
+
+def Saque(usuário, valor):
+    if usuários[usuário][0] < valor:
+        print("Impossível sacar, apenas R${:.2f} na conta".format(usuários[usuário][0]))
+    elif usuários[usuário][1] < valor:
+        print("Impossível sacar, limite de R${:.2f}".format(usuários[usuário][1]))
+    elif usuários[usuário][3] <= 0: #aqui está errado preciso configurar para considerar os saques realizados e comparar com o limite disponível
+        print("Impossível sacar, limites de saques atingidos")
+    else:
+        usuários[usuário][0] -= valor
+        usuários[usuário][0] #colocar para tirar do limite
+        usuários[usuário][2] = "\nSaque de R${:.2f}".format(valor, usuários[usuário][0])
+        print("Saque de R${:.2f} realizado. Resta R${:.2f} na conta".format(valor, usuários[usuário][0]))
+
+def Extrato(usuário):
+    print("-- Extrato --")
+    print(usuários[usuário][2])
+
+while True:
+    print("""Opções:
+          (a) Criar conta
+          (b) Depósito
+          (c) Saque
+          (d) Extrato
+""")
+    a = input("Insira a função desejada >")
